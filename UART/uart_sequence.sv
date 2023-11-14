61703fe19e66249ed37ed517eece4ca0a5bc2298
codebjjung <jbj970324@gmail.com>
2023-11-14 19:18:01 +0900

    add uvm sequence item

--- UART/uart_sequence.sv
+++ UART/uart_sequence.sv
@@ -4,6 +4,29 @@ typedef enum bit [3:0] {rand_baud_1_stop = 0, rand_length_1_stop = 1,
    rand_baud_2_stop = 11, rand_length_2_stop = 12
 } oper_mode;
 
+class transaction extends uvm_sequence_item;
+   `uvm_object_utils(transaction)
+
+   rand oper_mode op;
+   logic tx_start, rx_start;
+   logic rst;
+   rand logic [7:0] tx_data;
+   rand logic [16:0] baud;
+   rand logic [3:0] length;
+   rand logic parity_type, parity_en;
+   logic stop2;
+   logic tx_done, rx_done, tx_err, rx_err;
+   logic [7:0] rx_out;
+
+   constraint baud_c { baud inside {4800, 9600, 14400, 19200, 38400}; }
+   constraint length_c { length inside {5, 6, 7, 8}; }
+
+   function new(string name = "transaction");
+      super.new(name);
+   endfunction
+
+endclass : transaction
+
 class rand_baud extends uvm_sequence#(transaction);
    `uvm_object_utils(rand_baud)
 
