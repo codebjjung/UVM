c27d543ee744eceb5282cfebfc9ba64dc2149771
codebjjung <jbj970324@gmail.com>
2023-11-14 19:03:53 +0900

    typedef define

--- UART/uart_sequence.sv
+++ UART/uart_sequence.sv
@@ -1,3 +1,9 @@
+typedef enum bit [3:0] {rand_baud_1_stop = 0, rand_length_1_stop = 1,
+   length5wp = 2, length6wp = 3, length7wp = 4, length8wp = 5,
+   length5wop = 6, length6wop = 7, length7wop = 8, length8wop = 9,
+   rand_baud_2_stop = 11, rand_length_2_stop = 12
+} oper_mode;
+
 class rand_baud extends uvm_sequence#(transaction);
    `uvm_object_utils(rand_baud)
 
