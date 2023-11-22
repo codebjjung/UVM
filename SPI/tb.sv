58213d4dac5efada4b6cca904ba9ee405df48ce4
codebjjung <jbj970324@gmail.com>
2023-11-22 11:00:44 +0900

    spi testbench

---
+++ SPI/tb.sv
@@ -0,0 +1,42 @@
+`include "spi_cfg.sv"
+`include "spi_if.sv"
+`include "spi_seq.sv"
+`include "spi_cov.sv"
+`include "spi_mon.sv"
+`include "spi_sc.sv"
+`include "spi_drv.sv"
+`include "spi_agent.sv"
+`include "spi_env.sv"
+`include "spi_test.sv"
+
+module tb;
+
+spi_i vif();
+
+top dut (.wr(vif.wr),
+   .clk(vif.clk),
+   .rst(vif.rst),
+   .addr(vif.addr),
+   .din(vif.din),
+   .dout(vif.dout),
+   .done(vif.done),
+   .err(vif.err)
+);
+
+initial begin
+   vif.clk <= 0;
+end
+
+always #10 vif.clk <= ~vif.clk;
+
+initial begin
+   uvm_config_db#(virtual spi_i)::set(null, "*", "vif", vif);
+   run_test("test");
+end
+
+initial begin
+   $fsdbDumpfile("dump.fsdb");
+   $fsdbDumpvars;
+end
+
+endmodule
